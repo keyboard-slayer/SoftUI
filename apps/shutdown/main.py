d31a50d4d00573a1c6b1209e83a841112aedd5c6
0v3rl0w <dalcjor@libmail.eu>
2019-05-25 17:01:51 +0200

    Shutdown app fixed

--- apps/shutdown/main.py
+++ apps/shutdown/main.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import os
+import pygame
+from typing import Tuple
 
 class Shutdown:
-    def __init__(self):
-        self = self
+    def __init__(self, size: Tuple[int, int]):
+        self.screen_res = size
 
     def mainloop(self):
         if os.environ["dev"] == "dev":
@@ -13,6 +15,9 @@ class Shutdown:
         else:
             os.system("poweroff")
 
+    def get_surface() -> pygame.Surface:
+        return pygame.Display(self.screen_res)
 
-def callObject() -> object:
-    return Shutdown()
+
+def callObject(size) -> object:
+    return Shutdown(size)
